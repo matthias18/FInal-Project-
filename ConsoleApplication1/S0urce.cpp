edc5f39758f2ce04209f1df4914dfbe52e533c77
matthias18 <matthiascarroll@gmail.com>
2016-05-05 20:10:33 -0700

    Added files via upload

---
+++ ConsoleApplication1/S0urce.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include 'cl4sses'
+using namespace std;
+
+
+int main()
+{
+	cout << "Welcome to the easy, automatic grader program! \n";
+	cout << " "
+	return 0;
+}
